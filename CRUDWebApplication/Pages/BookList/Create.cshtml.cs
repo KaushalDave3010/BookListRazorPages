e58ba5a99e7fb6753aedbab4610370ad2395e8f4
Kaushal Dave <kndave3010@gmail.com>
2020-07-14 11:09:43 +0530

    Add project files.

---
+++ CRUDWebApplication/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUDWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRUDWebApplication.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+
+        private readonly ApplicationDBContext _db;
+
+        public CreateModel(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+
+        public Book Book { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<ActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Book.AddAsync(Book);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
